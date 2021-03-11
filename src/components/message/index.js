74689e0bf579f6c0e50b98e3a5e74d127b7793ee
Miguel Rubi <rsnakdmx@hotmail.com>
2021-03-10 23:54:32 -0600

    Carga inicial

---
+++ src/components/message/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Page from './page';
+
+class Message extends React.Component
+{
+    render()
+    {
+        return <Page />;
+    }
+}
+
+export default Message;
---
+++ src/components/survey/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Page from './page';
+
+class Survey extends React.Component
+{
+    render()
+    {
+        return <Page />;
+    }
+}
+
+export default Survey;
